0748a1c4c45eb6da998d13f8ae33b4579b6858de
Jetug <ksergeev01@gmail.com>
2025-01-29 23:42:41 +0300

    anim upd | added translation

--- src/main/java/com/nukateam/cgs/client/GatlingAnimator.java
+++ src/main/java/com/nukateam/cgs/client/GatlingAnimator.java
@@ -1,39 +1,62 @@
 package com.nukateam.cgs.client;
 
-import com.nukateam.cgs.Gunsmithing;
 import com.nukateam.geo.render.DynamicGeoItemRenderer;
 import com.nukateam.ntgl.client.animators.GunAnimator;
 import com.nukateam.ntgl.client.event.ClientHandler;
-import com.nukateam.ntgl.client.util.handler.ShootingData;
-import com.nukateam.ntgl.client.util.handler.ShootingHandler;
-import com.nukateam.ntgl.client.util.util.TransformUtils;
-import com.nukateam.ntgl.common.util.util.Cycler;
+import com.nukateam.ntgl.client.util.handler.ClientReloadHandler;
 import com.nukateam.ntgl.common.util.util.GunModifierHelper;
 import mod.azure.azurelib.core.animation.*;
 import mod.azure.azurelib.core.object.PlayState;
-import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemDisplayContext;
 
-import static com.nukateam.ntgl.client.util.util.TransformUtils.isRightHand;
+import static com.nukateam.example.common.util.constants.Animations.HOLD;
+import static com.nukateam.example.common.util.constants.Animations.SHOT;
+import static com.nukateam.ntgl.client.util.util.TransformUtils.isFirstPerson;
 import static mod.azure.azurelib.core.animation.Animation.LoopType.*;
 import static mod.azure.azurelib.core.animation.RawAnimation.begin;
 
 public class GatlingAnimator extends GunAnimator {
     public static final String HANDLE = "handle";
+    public static final String VOID = "void";
     protected final AnimationController<GunAnimator> HANDLE_CONTROLLER = createController("handle_controller", animateHandle());
+    protected final AnimationController<GunAnimator> GATLING_TRIGGER = createController( "gatling_trigger", event -> PlayState.CONTINUE)
+            .triggerableAnim(HANDLE, begin().then(HANDLE, PLAY_ONCE))
+            .triggerableAnim(VOID, begin().then(VOID, PLAY_ONCE));
 
     public GatlingAnimator(ItemDisplayContext transformType, DynamicGeoItemRenderer<GunAnimator> renderer) {
         super(transformType, renderer);
     }
 
     protected AnimationController.AnimationStateHandler<GunAnimator> animateHandle() {
-        return (event) -> getCycledAnimation(event, HANDLE, this.barrelCycler);
+//        return (event) -> getCycledAnimation(event, HANDLE, this.barrelCycler);
+        return event -> {
+            var controller = event.getController();
+            controller.setAnimationSpeed(1);
+            var entity = getEntity();
+            var arm = getArm();
+            var isShooting = shootingHandler.isShooting(entity, arm);
+            var animation = begin();
+            var rate = GunModifierHelper.getRate(getStack());
+
+            if (isShooting) {
+                aTicks = rate * getBarrelAmount();
+                animation.then(HANDLE, LOOP);
+                animationHelper.syncAnimation(event, HANDLE, rate * getBarrelAmount());
+            }
+            else if (aTicks == 0){
+                animation.then(VOID, PLAY_ONCE);
+            }
+
+            return event.setAndContinue(animation);
+        };
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
         super.registerControllers(controllerRegistrar);
         controllerRegistrar.add(HANDLE_CONTROLLER);
+        controllerRegistrar.add(GATLING_TRIGGER);
     }
 
     @Override
@@ -41,14 +64,26 @@ public class GatlingAnimator extends GunAnimator {
         return 4;
     }
 
+    protected int aTicks = 0;
+
     @Override
     public void tick() {
         super.tick();
-//        var fireTimer = GunModifierHelper.getFireDelay(getStack());
-//        var data = ShootingHandler.get().getShootingData(TransformUtils.getHand(transformType));
-//        if(fireTimer == data.fireTimer){
-//            barrelCycler.cycle();
+
+//        var entity = getEntity();
+//        var arm = getArm();
+//        var isShooting = shootingHandler.isShooting(entity, arm);
+//        var rate = GunModifierHelper.getRate(getStack());
+
+        aTicks = Math.max(aTicks - 1, 0);
+
+//        if(isShooting && !GATLING_TRIGGER.isPlayingTriggeredAnimation()){
+//            var ratio = animationHelper.getSpeedMultiplier(HANDLE, rate * getBarrelAmount() - 3);
+//            GATLING_TRIGGER.setAnimationSpeed(ratio);
+//            GATLING_TRIGGER.tryTriggerAnimation(HANDLE);
 //        }
-//        Gunsmithing.LOGGER.info("barrel id: " + barrelCycler.getCurrent());
+//        else if(!isShooting){
+//            GATLING_TRIGGER.tryTriggerAnimation("void");
+//        }
     }
 }
