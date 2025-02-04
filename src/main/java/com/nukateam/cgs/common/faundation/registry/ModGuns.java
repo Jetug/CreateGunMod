85ff194ab81d98e95506bec045e3a1c0c3952002
Jetug <ksergeev01@gmail.com>
2025-02-04 23:32:42 +0300

    visual fixes

--- src/main/java/com/nukateam/cgs/common/faundation/registry/ModGuns.java
+++ src/main/java/com/nukateam/cgs/common/faundation/registry/ModGuns.java
@@ -14,14 +14,13 @@ import net.minecraftforge.registries.*;
 public class ModGuns {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Gunsmithing.MOD_ID);
 
+//    public static final RegistryObject<GunItem> MACHINE_GUN = registerGun("machine_gun");
+//    public static final RegistryObject<GunItem> FLAMER = registerGun("flamethrower");
+
     ///GUNS
     public static final RegistryObject<GunItem> REVOLVER = registerGun("revolver");
-    public static final RegistryObject<GunItem> MACHINE_GUN = registerGun("machine_gun");
-    public static final RegistryObject<GunItem> FLAMER = registerGun("flamethrower");
     public static final RegistryObject<GunItem> GATLING = ITEMS.register("gatling",
             () -> new GatlingItem(new Item.Properties().stacksTo(1)));
-
-
 
     public static RegistryObject<GunItem> registerGun(String name) {
         return ITEMS.register(name, () -> new BaseGunItem(new Item.Properties().stacksTo(1)));
