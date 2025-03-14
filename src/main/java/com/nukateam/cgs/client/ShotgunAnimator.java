cc1b956f58223d5cdd8b737232ae3c3fd342cc92
Jetug <ksergeev01@gmail.com>
2025-03-14 23:12:32 +0300

    save

--- src/main/java/com/nukateam/cgs/client/ShotgunAnimator.java
+++ src/main/java/com/nukateam/cgs/client/ShotgunAnimator.java
@@ -4,77 +4,34 @@ import com.nukateam.cgs.common.faundation.registry.AttachmentItems;
 import com.nukateam.cgs.common.ntgl.CgsAttachmentTypes;
 import com.nukateam.geo.render.DynamicGeoItemRenderer;
 import com.nukateam.ntgl.client.animators.GunAnimator;
+import com.nukateam.ntgl.client.util.handler.ClientReloadHandler;
 import com.nukateam.ntgl.common.data.config.gun.Gun;
 import com.nukateam.ntgl.common.foundation.item.GunItem;
 import com.nukateam.ntgl.common.util.util.GunModifierHelper;
-import mod.azure.azurelib.core.animation.AnimationController;
-import mod.azure.azurelib.core.animation.AnimationState;
-import mod.azure.azurelib.core.animation.RawAnimation;
+import mod.azure.azurelib.core.animation.*;
+import mod.azure.azurelib.core.object.PlayState;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraftforge.event.TickEvent;
 
+import static com.nukateam.example.common.util.constants.Animations.RELOAD;
 import static mod.azure.azurelib.core.animation.Animation.LoopType.LOOP;
 import static mod.azure.azurelib.core.animation.Animation.LoopType.PLAY_ONCE;
 import static mod.azure.azurelib.core.animation.RawAnimation.begin;
 
 public class ShotgunAnimator extends GunAnimator {
-    private boolean hasBelt;
-    private boolean isAuto;
+    protected final AnimationController<GunAnimator> COCK_CONTROLLER;
 
-//    protected final AnimationController<GunAnimator> BELT_CONTROLLER = createController("belt_controller", animateBelt());
-//
-//    private AnimationController.AnimationStateHandler<GunAnimator> animateBelt() {
-//
-//    }
+    private int ammo;
 
     public ShotgunAnimator(ItemDisplayContext transformType, DynamicGeoItemRenderer<GunAnimator> renderer) {
         super(transformType, renderer);
+        COCK_CONTROLLER = createController("cock_controller", animateCock());
     }
 
     @Override
-    protected AnimationController.AnimationStateHandler<GunAnimator> animateRevolver() {
-        if(hasBelt){
-            return (event) ->{
-                var animation = begin()
-                        .then("belt", PLAY_ONCE)
-                        .then("void", PLAY_ONCE);
-
-                animationHelper.syncAnimation(event, "belt", rate);
-                return event.setAndContinue(animation);
-            };
-        }
-        else return super.animateRevolver();
-    }
-
-    @Override
-    protected RawAnimation getHoldAnimation(AnimationState<GunAnimator> event) {
-        if(isAuto){
-            return playGunAnim("hold_auto", LOOP);
-        }
-        return super.getHoldAnimation(event);
-    }
-
-    @Override
-    protected RawAnimation getShootingAnimation(AnimationState<GunAnimator> event) {
-        if(isAuto){
-            var animation = playGunAnim("shot_auto", LOOP);
-            var rate = GunModifierHelper.getRate(getStack());
-            animationHelper.syncAnimation(event, "shot_auto", rate);
-            return animation;
-        }
-        return super.getShootingAnimation(event);
-    }
-
-    @Override
-    protected RawAnimation getReloadingAnimation(AnimationState<GunAnimator> event) {
-        if(hasBelt){
-            var animation = begin();
-            animation.then("reload_belt", LOOP);
-            animationHelper.syncAnimation(event, "reload_belt", GunModifierHelper.getReloadTime(getStack()));
-
-            return animation;
-        }
-        return super.getReloadingAnimation(event);
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
+        super.registerControllers(controllerRegistrar);
+        controllerRegistrar.add(COCK_CONTROLLER);
     }
 
     @Override
@@ -82,13 +39,29 @@ public class ShotgunAnimator extends GunAnimator {
         super.tick(event);
         if (event.phase == TickEvent.Phase.START) {
             if (isGun()) {
-                var chamberAttachment = Gun.getAttachmentItem(CgsAttachmentTypes.CHAMBER, getStack());
-                var frame = Gun.getAttachmentItem(CgsAttachmentTypes.FRAME, getStack());
-
-                this.hasBelt = chamberAttachment.is(AttachmentItems.BELT.get());
-                this.isAuto = frame.is(AttachmentItems.AUTO.get());
+                this.ammo = Gun.getAmmo(getStack());
             }
         }
+    }
+
+    private AnimationController.AnimationStateHandler<GunAnimator> animateCock() {
+        return (event) -> {
+            var name = "";
+            switch (ammo){
+                case 0:
+                    name = "empty_both";
+                    break;
+                case 1:
+                    animation.then("empty_left", LOOP);
+                    break;
+                case 2:
+                    animation.then("full", LOOP);
+                    break;
+            }
+
+            var animation = begin().then("full", LOOP);
+            return event.setAndContinue(animation);
+        };
     }
 
     private boolean isGun() {
