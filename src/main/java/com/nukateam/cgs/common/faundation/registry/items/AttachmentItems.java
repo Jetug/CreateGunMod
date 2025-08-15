8040fbd06dd80bf90f04d8bcc8201df6453c1b9a
Jetug <ksergeev01@gmail.com>
2025-08-15 22:34:36 +0300

    new icons

--- src/main/java/com/nukateam/cgs/common/faundation/registry/items/AttachmentItems.java
+++ src/main/java/com/nukateam/cgs/common/faundation/registry/items/AttachmentItems.java
@@ -33,15 +33,19 @@ public class AttachmentItems {
     public static final RegistryObject<Item> STOCK = ITEMS.register("stock",
             () -> new AttachmentItem<>(AttachmentType.STOCK, GenericAttachment.create(Attachments.STOCK), ITEM_PROPERTIES));
 
+    @ItemModelGen(path = "attachments")
+    public static final RegistryObject<Item> BAYONET = ITEMS.register("bayonet",
+            () -> new AttachmentItem<>(AttachmentType.MELEE,
+                    GenericAttachment.create(Attachments.BAYONET_MODIFIERS), ITEM_PROPERTIES));
+
     //FLINTLOCK
-//    @ItemModelGen(path = "attachments")
+    @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> FLINTLOCK_LONG_BARREL = ITEMS.register("flintlock_long_barrel",
             () -> new BarrelItem(Barrel.create(11f, Attachments.LONG_BARREL), ITEM_PROPERTIES));
 
-
-    public static final RegistryObject<Item> FLINTLOCK_BAYONET = ITEMS.register("flintlock_bayonet",
-            () -> new AttachmentItem<>(AttachmentType.MELEE,
-                    GenericAttachment.create(Attachments.BAYONET_MODIFIERS), ITEM_PROPERTIES));
+    @ItemModelGen(path = "attachments")
+    public static final RegistryObject<Item> FLINTLOCK_MORTAR_BARREL = ITEMS.register("flintlock_mortar_barrel",
+            () -> new BarrelItem(Barrel.create(11f, Attachments.LONG_BARREL), ITEM_PROPERTIES));
 
     //REVOLVER
     @ItemModelGen(path = "attachments")
@@ -52,7 +56,7 @@ public class AttachmentItems {
     public static final RegistryObject<Item> REVOLVER_BELT = ITEMS.register("round_belt",
             () -> new AttachmentItem<>(CgsAttachmentTypes.CHAMBER,
                     GenericAttachment.create(BELT_MODIFIERS), ITEM_PROPERTIES));
- 
+
     @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> REVOLVER_AUTO = ITEMS.register("auto_fire",
             () -> new AttachmentItem<>(CgsAttachmentTypes.FRAME,
@@ -97,12 +101,47 @@ public class AttachmentItems {
             () -> new BarrelItem(Barrel.create(1f, Attachments.NAILGUN_SPLIT_BARREL), ITEM_PROPERTIES));
 
     //FLAMETHROWER
-//    @ItemModelGen(path = "attachments")
+    @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> POTION_TANK = ITEMS.register("potion_tank",
             () -> new AttachmentItem<>(CgsAttachmentTypes.FRAME,
                     GenericAttachment.create(), ITEM_PROPERTIES));
 
+    //LAUNCHER
+//    @ItemModelGen(path = "attachments")
+//    public static final RegistryObject<Item> ROCKET_CONTAINER = ITEMS.register("rocket_container",
+//            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
+//                    GenericAttachment.create(), ITEM_PROPERTIES));
+
+    @ItemModelGen(path = "attachments")
+    public static final RegistryObject<Item> AUTO_LAUNCHER = ITEMS.register("launcher_auto",
+            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
+                    GenericAttachment.create(), ITEM_PROPERTIES));
+
+    @ItemModelGen(path = "attachments")
+    public static final RegistryObject<Item> BALLISTAZOOKA = ITEMS.register("ballistazooka",
+            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
+                    GenericAttachment.create(), ITEM_PROPERTIES));
+
+    @ItemModelGen(path = "attachments")
+    public static final RegistryObject<Item> HOOK_LAUNCHER = ITEMS.register("launcher_hook",
+            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
+                    GenericAttachment.create(), ITEM_PROPERTIES));
+
+    @ItemModelGen(path = "attachments")
+    public static final RegistryObject<Item> ITEM_LAUNCHER = ITEMS.register("item_launcher",
+            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
+                    GenericAttachment.create(), ITEM_PROPERTIES));
+
+    @ItemModelGen(path = "attachments")
+    public static final RegistryObject<Item> LAUNCHER_BAYONET = ITEMS.register("big_bayonet",
+            () -> new AttachmentItem<>(AttachmentType.MUZZLE,
+                    GenericAttachment.create(), ITEM_PROPERTIES));
+
     //HAMMER
+    @ItemModelGen(path = "attachments")
+    public static final RegistryObject<Item> SHOTGUN_RECEIVER = ITEMS.register("shotgun_receiver",
+            () -> new AttachmentItem<>(CgsAttachmentTypes.FRAME, GenericAttachment.create(), ITEM_PROPERTIES));
+
     @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> HAMMER_STONE = ITEMS.register("hammer_stone",
             () -> new HammerHeadItem(Tiers.STONE, HammerHeadItem.Type.HAMMER, GenericAttachment.create(), new Item.Properties()));
@@ -111,59 +150,30 @@ public class AttachmentItems {
     public static final RegistryObject<Item> HAMMER_IRON = ITEMS.register("hammer_iron",
             () -> new HammerHeadItem(Tiers.IRON, HammerHeadItem.Type.HAMMER, GenericAttachment.create(), new Item.Properties()));
 
+    @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> HAMMER_DIAMOND = ITEMS.register("hammer_diamond",
             () -> new HammerHeadItem(Tiers.DIAMOND, HammerHeadItem.Type.HAMMER, GenericAttachment.create(), new Item.Properties()));
 
+    @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> HAMMER_NETHERITE = ITEMS.register("hammer_netherite",
             () -> new HammerHeadItem(Tiers.NETHERITE, HammerHeadItem.Type.HAMMER, GenericAttachment.create(), new Item.Properties()));
 
     //
+    @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> AXE_STONE = ITEMS.register("axe_stone",
             () -> new HammerHeadItem(Tiers.STONE, HammerHeadItem.Type.AXE, GenericAttachment.create(), new Item.Properties()));
 
+    @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> AXE_IRON = ITEMS.register("axe_iron",
             () -> new HammerHeadItem(Tiers.IRON, HammerHeadItem.Type.AXE, GenericAttachment.create(), new Item.Properties()));
 
+    @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> AXE_DIAMOND = ITEMS.register("axe_diamond",
             () -> new HammerHeadItem(Tiers.DIAMOND, HammerHeadItem.Type.AXE, GenericAttachment.create(), new Item.Properties()));
 
+    @ItemModelGen(path = "attachments")
     public static final RegistryObject<Item> AXE_NETHERITE = ITEMS.register("axe_netherite",
             () -> new HammerHeadItem(Tiers.NETHERITE, HammerHeadItem.Type.AXE, GenericAttachment.create(), new Item.Properties()));
-
-
-    public static final RegistryObject<Item> SHOTGUN_RECEIVER = ITEMS.register("shotgun_receiver",
-            () -> new AttachmentItem<>(CgsAttachmentTypes.FRAME, GenericAttachment.create(), ITEM_PROPERTIES));
-
-    //LAUNCHER
-//    @ItemModelGen(path = "attachments")
-    public static final RegistryObject<Item> ROCKET_CONTAINER = ITEMS.register("rocket_container",
-            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
-                    GenericAttachment.create(), ITEM_PROPERTIES));
-
-//    @ItemModelGen(path = "attachments")
-    public static final RegistryObject<Item> AUTO_LAUNCHER = ITEMS.register("auto_launcher",
-            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
-                    GenericAttachment.create(), ITEM_PROPERTIES));
-
-//    @ItemModelGen(path = "attachments")
-    public static final RegistryObject<Item> BALLISTAZOOKA = ITEMS.register("ballistazooka",
-            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
-                    GenericAttachment.create(), ITEM_PROPERTIES));
-
-//    @ItemModelGen(path = "attachments")
-    public static final RegistryObject<Item> GRAPPLING_HOOK = ITEMS.register("grappling_hook",
-            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
-                    GenericAttachment.create(), ITEM_PROPERTIES));
-
-//    @ItemModelGen(path = "attachments")
-    public static final RegistryObject<Item> ITEM_LAUNCHER = ITEMS.register("item_launcher",
-            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
-                    GenericAttachment.create(), ITEM_PROPERTIES));
-
-//    @ItemModelGen(path = "attachments")
-    public static final RegistryObject<Item> LAUNCHER_BAYONET = ITEMS.register("launcher_bayonet",
-            () -> new AttachmentItem<>(AttachmentType.MAGAZINE,
-                    GenericAttachment.create(), ITEM_PROPERTIES));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
