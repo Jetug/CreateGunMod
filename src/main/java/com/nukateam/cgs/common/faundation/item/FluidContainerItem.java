342e920539fecaf68535fb1c69580ea325b710ac
Jetug <ksergeev01@gmail.com>
2026-01-07 17:26:25 +0300

    fluid container fix 5

--- src/main/java/com/nukateam/cgs/common/faundation/item/FluidContainerItem.java
+++ src/main/java/com/nukateam/cgs/common/faundation/item/FluidContainerItem.java
@@ -15,6 +15,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
@@ -22,6 +23,7 @@ import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.BucketPickup;
+import net.minecraft.world.level.block.CauldronBlock;
 import net.minecraft.world.level.block.LiquidBlockContainer;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -38,11 +40,11 @@ import javax.annotation.Nullable;
 import java.util.Optional;
 import java.util.function.Supplier;
 
-public class FluidContainerItem extends Item {
+public class FluidContainerItem extends BucketItem {
     private final Supplier<? extends Fluid> fluidSupplier;
 
     public FluidContainerItem(Supplier<? extends Fluid> fluid, Properties properties) {
-        super(properties);
+        super(fluid, properties);
         this.fluidSupplier = fluid;
     }
 
@@ -89,7 +91,6 @@ public class FluidContainerItem extends Item {
             var fluid = state.getFluidState().getType();
 
             if (this.fluidSupplier.get() == Fluids.EMPTY) {
-
                 var pickupResult = tryPickupFluid(player, level, hitResult, fluid);
                 if (pickupResult.consumesAction()) {
                     var container = handleContainerAfterUse(fluid, player, stack, true);
