0ce8e7d42ad6a4a46b3c9e050106badd25cd5ede
Jetug <ksergeev01@gmail.com>
2026-07-17 23:15:47 +0300

    new ntgl adaptation

--- src/main/java/com/nukateam/cgs/client/renderers/weapon/BaseWeaponRenderer.java
+++ src/main/java/com/nukateam/cgs/client/renderers/weapon/BaseWeaponRenderer.java
@@ -17,7 +17,7 @@ import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
-public class BaseWeaponRenderer extends DynamicWeaponRenderer {
+public class BaseWeaponRenderer extends DynamicWeaponRenderer<WeaponAnimator> {
     public BaseWeaponRenderer() {
         super(new GeoWeaponModel());
     }
